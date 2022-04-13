26c5af8358490298da87248305e001edb95432c0
Alex Ali <alexali00@yahoo.com>
2022-04-12 23:50:43 -0500

    gitignore shit

---
+++ SW/Motor.h
@@ -0,0 +1,12 @@
+//------------Motor_Init------------
+// Initialize driver for motor
+// 
+// Input: none
+// Output: none
+void Motor_Init(void);
+
+//------------Motor_ChangeSpeed------------
+// 
+// Input: int change
+// Output: none
+void Motor_ChangeSpeed(int change);
---
+++ SW/Tachometer.h
@@ -0,0 +1,13 @@
+//------------Tachometer_Init------------
+// Initialize input capture for tachometer using Timer0A
+// 
+// Input: none
+// Output: none
+void Tachometer_Init(void);
+
+//------------Tachometer_Period------------
+// Output the current period measured by the tachometer
+// 
+// Input: none
+// Output: current period
+int Tachometer_Period(void);
